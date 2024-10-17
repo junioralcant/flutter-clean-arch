de94488a264c238abe726395119b5ed56ec7ef36
user <user@MB033468.local>
2024-10-17 19:05:33 -0300

    refactor: change strategy to use factory constructor

--- test/next_event_player_test.dart
+++ test/next_event_player_test.dart
@@ -3,23 +3,39 @@ import 'package:flutter_test/flutter_test.dart';
 class NextEventPlayer {
   final String id;
   final String name;
-  late final initials;
+  final String initials;
   final String? photo;
   final String? position;
   final bool isConfirmed;
   final DateTime? confirmationDate;
 
-  NextEventPlayer(
+  NextEventPlayer._(
       {required this.id,
       required this.name,
       required this.isConfirmed,
+      required this.initials,
       this.photo,
       this.position,
-      this.confirmationDate}) {
-    initials = _getInitials();
-  }
+      this.confirmationDate});
 
-  String _getInitials() {
+  factory NextEventPlayer({
+    required String id,
+    required String name,
+    required bool isConfirmed,
+    String? photo,
+    String? position,
+    DateTime? confirmationDate,
+  }) =>
+      NextEventPlayer._(
+          id: id,
+          name: name,
+          isConfirmed: isConfirmed,
+          initials: _getInitials(name),
+          confirmationDate: confirmationDate,
+          photo: photo,
+          position: photo);
+
+  static String _getInitials(String name) {
     final names = name.split(' ');
     final firstChar = names.first[0];
     final lastChar = names.last[0];
